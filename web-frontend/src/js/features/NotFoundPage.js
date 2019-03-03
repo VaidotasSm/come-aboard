afa22bea398a8c7ddba047f33f3e3ff378b82497
Arūnas Smalskys <arunas.smalskys@outlook.com>
2019-03-03 17:02:20 +0200

    Center not found page

--- web-frontend/src/js/features/NotFoundPage.js
+++ web-frontend/src/js/features/NotFoundPage.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default function NotFoundPage(props) {
 	return (
 		<div>
-			<div className="title is-2">Page not found</div>
+			<div className="title is-2 has-text-centered">Page not found</div>
 		</div>
 	);
 }
